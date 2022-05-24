cd86b4b79f7c737484f6cfb371cb9633830f88f5
Rhon Umahon <65482394+rhonumahon@users.noreply.github.com>
2022-05-24 16:17:42 +0100

    Setup DB mockdata and add main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Friendster';
+
+  constructor(){
+    console.log(new Date(2018, 11, 24, 10, 33));
+    
+  }
+  
 }
